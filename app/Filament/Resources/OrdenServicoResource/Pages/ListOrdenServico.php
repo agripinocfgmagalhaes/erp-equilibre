eac1412159a40407ed9adeea0adfa55d2771fd18
Equilibre ERP <admin@equilibreconstrucoes.com.br>
2026-08-03 22:51:17 +0000

    feat: aplica padrao de largura maxima e resize em Ordens de Servico

--- app/Filament/Resources/OrdenServicoResource/Pages/ListOrdenServico.php
+++ app/Filament/Resources/OrdenServicoResource/Pages/ListOrdenServico.php
@@ -1,11 +1,16 @@
 <?php
 namespace App\Filament\Resources\OrdenServicoResource\Pages;
+use Filament\Support\Enums\Width;
+use Asmit\ResizedColumn\HasResizableColumn;
 use App\Filament\Resources\OrdenServicoResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Hydrat\TableLayoutToggle\Concerns\HasToggleableTable;
 class ListOrdenServico extends ListRecords
 {
+    use HasToggleableTable, HasResizableColumn;
     protected static string $resource = OrdenServicoResource::class;
+    protected Width|string|null $maxContentWidth = 'full';
     protected function getHeaderActions(): array
     {
         return [CreateAction::make()->label('+ Ordem de Serviço')->slideOver()];
